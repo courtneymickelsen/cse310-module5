39b0b7b349042348194f03a4c46064de9d407003
courtneymickelsen <lam15020@byui.edu>
2022-03-22 12:26:21 -0600

    almost done!

--- clients.py
+++ clients.py
@@ -3,25 +3,6 @@ class Clients():
         self.fname = fname
         self.lname = lname
         self.length = 0
-        self.type = ""
-        self.last_appt = ""
+        self.type = "Unknown"
+        self.last_appt = "00-00-0000"
         self.last_payment = 0.00
-
-    def set_length(self, length):
-        self.length = length
-        # update length in database
-
-    def set_type(self, type):
-        self.type = type
-        # update type in database
-
-    def set_last_appt(self, last_appt):
-        self.last_appt = last_appt
-        # update last_appt in database
-
-    def set_last_payment(self, last_payment):
-        self.last_payment = last_payment
-        # update last_payment in database
-
-    def view_all_info(self):
-        c.execute("SELECT * WHERE (fname=:fname AND lname = lname)", {'fname': self.fname, 'lname': self.lname})
